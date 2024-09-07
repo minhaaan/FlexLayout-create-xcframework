6edd820501bd2d16ee9b8f3c9f797217a6fb0308
최민한 <chmh0411@gmail.com>
2024-09-07 19:40:36 +0900

    add: PinLayout.sh

---
+++ PinLayout.sh
@@ -0,0 +1,37 @@
+# 버전 입력 받기
+echo "🚀 Input PinLayout version ex) 1.10.5"
+read version
+
+rm -rf PinLayout.zip
+rm -rf PinLayout
+rm -rf PinLayout.xcframework
+
+# download
+curl https://github.com/layoutBox/PinLayout/archive/refs/tags/$version.zip -L -o PinLayout.zip
+unzip PinLayout.zip
+mv PinLayout-$version PinLayout
+
+cd PinLayout
+
+for SDK in iphoneos iphonesimulator
+do
+xcodebuild archive \
+  -workspace PinLayout.xcworkspace \
+  -scheme PinLayout-iOS \
+  -archivePath "PinLayout-$SDK.xcarchive" \
+  -sdk $SDK \
+  SKIP_INSTALL=NO \
+  BUILD_LIBRARY_FOR_DISTRIBUTION=YES \
+  DEBUG_INFORMATION_FORMAT=DWARF \
+  MARKETING_VERSION=$version
+done
+
+xcodebuild -create-xcframework \
+  -framework "PinLayout-iphoneos.xcarchive/Products/Library/Frameworks/PinLayout.framework" \
+  -framework "PinLayout-iphonesimulator.xcarchive/Products/Library/Frameworks/PinLayout.framework" \
+  -output "../PinLayout.xcframework"
+
+cd ..
+
+rm -rf PinLayout.zip
+rm -rf PinLayout
